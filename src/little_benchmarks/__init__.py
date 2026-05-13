03f0c3cd5915d95b6c433488370e64d65badf56b
McDougall <Duncan.McDougall@rfi.ac.uk>
2026-05-13 08:54:52 +0100

    Add license information

--- src/little_benchmarks/__init__.py
+++ src/little_benchmarks/__init__.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2026 Duncan McDougall <duncan.mcdougall@rfi.ac.uk>
+#
+# SPDX-License-Identifier: Apache-2.0
+
 from ms_nexus_tools.lib.utils import format_bytes
 import time
 from typing import Any
